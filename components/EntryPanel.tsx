c488dc7f281496b35ac67036a14c0669ca170c84
Berat Çimen <beratcmn@gmail.com>
2022-06-25 17:44:05 +0300

    Fixed spacing issue

--- components/EntryPanel.tsx
+++ components/EntryPanel.tsx
@@ -1,7 +1,7 @@
 export default function EntryPanel() {
   return (
     <div className="flex flex-row justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
-      <div className="px-10 gap-y-5">
+      <div className="px-10 space-y-5">
         <h1 className="text-6xl max-w-xl font-serif">
           <span className="underline decoration-black decoration-4">
             Medium
