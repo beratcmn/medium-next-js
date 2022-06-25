1136f2eccf8c988e30088be705d28667de1c4cb9
Berat Çimen <beratcmn@gmail.com>
2022-06-25 17:12:23 +0300

    Custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,8 @@
+export default function ForOfOr() {
+  return (
+    <div className="h-screen w-full flex flex-col items-center justify-center gap-y-5">
+      <h1 className="font-bold text-3xl">404</h1>
+      <p className="text-lg">Aradığınız sayfa bulunamadı!</p>
+    </div>
+  );
+}
